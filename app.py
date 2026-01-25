edadf26c979f041908494b57fb7453809aacff40
calebjubal <2301730055@krmu.edu.in>
2026-01-25 08:38:13 +0530

    feat: implement PDF text extraction and processing in Streamlit app

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    return text_splitter.split_text(text)
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="My Streamlit App", layout="wide")
 
     st.header("Chat with multiple Documents :books: all at once!!!!!")
@@ -8,8 +26,17 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your documents here", accept_multiple_files=True)
-        st.button("Process")
+        pdf_docs = st.file_uploader(
+            "Upload your documents here", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
 
 if __name__ == "__main__":
     main()
